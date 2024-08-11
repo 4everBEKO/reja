351ed155c28c6166437d66859f791e9ddf17c64a
MICHAEL-1319 <4everbek@gmail.com>
2024-08-11 16:23:44 +0500

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -1,32 +1,44 @@
-console.log('Web serverni boshlash')
-const express = require("express")
-const app = express()
-const http = require('http')
+console.log("Web serverni boshlash");
+const express = require("express");
+const app = express();
+const http = require("http");
+const fs = require("fs");
 
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+    if (err) {
+        console.log("ERROR: ", err);
+    } else {
+        user = JSON.parse(data);
+    }
+});
 
 // 1: Kirish code
-app.use(express.static('public'))
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // 2: Session
 // 3: Views code
-app.set('views', 'views')
-app.set('view engine', 'ejs')
+app.set("views", "views");
+app.set("view engine", "ejs");
 
 // 4: Routing code
-app.post('/create-item', (req, res) => {
-    console.log(req.body)
-    res.json({ test: 'success' })
-})
+app.post("/create-item", (req, res) => {
+    // TODO: code with db here
+});
 
-app.get('/', function(req, res) {
-    res.render('harid')
-})
+app.get("/author", (req, res) => {
+    res.render("author", { user: user });
+});
 
-const server = http.createServer(app)
+app.get("/", function (req, res) {
+    res.render("harid");
+});
+
+const server = http.createServer(app);
 let PORT = 3000;
 
 server.listen(PORT, () => {
-    console.log(`The server is running successfully on port: ${PORT}`)
-})
+    console.log(`The server is running successfully on port: ${PORT}`);
+});
