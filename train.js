d31f9f6655edf8b9cf1df651eddae316f67cb988
MICHAEL-1319 <4everbek@gmail.com>
2024-08-18 10:27:14 +0500

    feat: train callback and async functions

--- train.js
+++ train.js
@@ -10,22 +10,64 @@ const list = [
 ]
 
 function maslahatBering(a, callback) {
-    if(typeof a !== "number") callback("insert a number", null)
+    if(typeof a!== "number") callback("insert a number", null)
     else if(a <= 20) callback(null, list[0])
     else if(a > 20 && a <= 30) callback(null, list[1])
     else if(a > 30 && a <= 40) callback(null, list[2])
     else if(a > 40 && a <= 50) callback(null, list[3])
     else if(a > 50 && a <= 60) callback(null, list[4])
     else {
-        setTimeout(() => {
-        callback(null, list[5])
-        }, 5000);
+        setInterval(() => {
+            callback(null, list[5])
+        }, 1000);
     }
-} 
+}
 
-console.log("passed here 0")
-maslahatBering(61, (err, data) => {
+console.log('passed here 0')
+maslahatBering(70, (err, data) => {
     if(err) console.log("ERROR:", err)
-        else console.log("Answer:" ,data)
+    console.log("Answer:", data)
 })
-console.log("passed here 1")
+console.log('passed here 1')
+
+
+
+// ASYNC function
+// async function maslahatBering(a) {
+//     if(typeof a !== "number") throw new Error("insert a number")
+//     else if(a <= 20) return list[0]
+//     else if(a > 20 && a <= 30) return list[1]
+//     else if(a > 30 && a <= 40) return list[2]
+//     else if(a > 40 && a <= 50) return list[3]
+//     else if(a > 50 && a <= 60) return list[4]
+//     else {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 resolve(list[5])
+//             }, 5000);
+//         })
+//     }
+// } 
+
+// call via then / catch
+// console.log("passed here 0")
+// maslahatBering(25)
+// .then((data) => {
+//     console.log("Answer:", data)
+// })
+// .catch((err) => {
+//     console.log("ERROR:", err)
+// })
+// console.log("passed here 1")
+
+// call via async / await
+// async function run() {
+//     let result = await maslahatBering(65)
+//     console.log(result)
+//     result = await maslahatBering(61)
+//     console.log(result)
+//     result = await maslahatBering(41)
+//     console.log(result)
+// }
+
+// run()
