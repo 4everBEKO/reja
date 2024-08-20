182f9949d6f80790fce9b21344e9062acf69f5b5
MICHAEL-1319 <4everbek@gmail.com>
2024-08-20 10:50:18 +0500

    fix: refactoring

--- server.js
+++ server.js
@@ -33,12 +33,12 @@ app.get("/author", (req, res) => {
 });
 
 app.get("/", function (req, res) {
-    res.render("harid");
+    res.render("reja");
 });
 
 const server = http.createServer(app);
 let PORT = 3000;
 
 server.listen(PORT, () => {
-    console.log(`The server is running successfully on port: ${PORT}`);
+    console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
 });
